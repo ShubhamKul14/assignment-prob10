d0d441d2f4a85ebc590298f2b4cd2ed19f10a535
ShubhamKulkarni14 <shubhamsk1497@gmail.com>
2020-09-05 19:38:25 +0530

    first code

---
+++ day10/coinSimulator.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+echo
+a=1
+h_c=0
+l_c=0
+n=30
+for ((i=1;i<=$n;i++))
+do
+flip=$(($(($RANDOM%10))%2))
+if [ $flip -eq 1 ]; then
+        h_c=$(($h_c+1))
+                        if [ $h_c -eq 21 ]
+                        then
+                                echo -e "\n○ HEADS IS WINNER , WON ○"
+                                echo -e "\t--$h_c times--\n"
+                                echo "HEADS WON BY : $(($h_c-$l_c)) times"
+                        break
+                        fi
+else
+        l_c=$(($l_c+1))
+                        if [ $l_c -eq 21 ]
+                        then
+                                echo -e "\n○ TAILS IS WINNER , WON ○"
+                                echo -e "\t--$l_c times--\n"
+                                echo "TAILS WON BY : $(($l_c-$h_c)) times"
+                        break
+                        fi
+fi
+if [ $i == $n ] && ( [ $h_c -lt 21 ] || [  $l_c -lt  21 ] )
+then
+        echo -e "\nTIE #$a"
+                echo -e "\nTIE HEADS COUNT : " $h_c
+                echo -e "\nTIE TAILS COUNT : " $l_c
+        n=$(($n+2))
+        a=$(($a+1))
+fi
+
+done
+
+echo
+echo "HEADS COUNT : " $h_c
+echo "TAILS COUNT : " $l_c
+echo
---
+++ day9/emloyewage.sh
@@ -0,0 +1,70 @@
+#! /bin/bash 
+
+	echo "Welcome to Employee Wage Computation"
+
+	TotalWage=0
+	TotalWorkingHrs=100
+	EmpWorkingHrs=0
+	WorkingDays=0
+	declare -A DailyWage_with_Day
+
+	function getWorkingHrs()
+	{
+		echo "Employee Work For $EmpWorkingHrs hr In This Month"
+	}
+
+	function  getDailyWageWithDate()
+	{
+		for day in ${!DailyWage_with_Day[@]}
+		do
+			echo "Day $day : ${DailyWage_with_Day[$day]}"
+		done
+	}
+
+
+	for((day=1;day<=20;day++))
+	do
+
+		EmpAttendance=$((RANDOM%3))
+
+		EmpWagePerHr=20
+
+		case $EmpAttendance in
+
+		0) WorkingHrs=0	# Employee is Absent
+		;;
+
+		1)	WorkingHrs=4 	# Employee is Present for Half Day
+			((WorkingDays++))
+			((EmpWorkingHrs+=WorkingHrs))
+		;;
+
+		2)	WorkingHrs=8	# Employee Work For Full Day
+			((WorkingDay++))
+			((EmpWorkingHrs+=WorkingHrs))
+		;;
+
+		esac
+
+		DailyWage=$(($WorkingHrs*$EmpWagePerHr))
+
+		DailyWageData+=($DailyWage)
+
+		DailyWage_with_Day[$day]=$DailyWage
+
+		((TotalWage+=DailyWage))
+
+		if [ $EmpWorkingHrs -ge $TotalWorkingHrs ]
+		then
+			break
+		fi
+
+	done
+
+	getWorkingHrs
+
+	echo "Employee earn Rs.$TotalWage ,this Month"
+
+	echo "Employee Daily Wage Data is ${DailyWageData[@]}"
+
+	getDailyWageWithDate
